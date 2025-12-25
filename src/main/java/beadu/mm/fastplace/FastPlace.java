b5db0919e912f5db66f9bd49a5fc6630608fc01f
unknown <BeaduMakesMods>
2025-12-25 17:54:45 +0200

    test

--- src/main/java/beadu/mm/fastplace/FastPlace.java
+++ src/main/java/beadu/mm/fastplace/FastPlace.java
@@ -1,15 +1,15 @@
-package com.brag.fastplace;
+package beadu.mm.fastplace;
 
-import com.brag.fastplace.mixin.MinecraftClientAccessor;
+import beadu.mm.fastplace.mixin.MinecraftClientAccessor;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
 
 public class FastPlace implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client != null && client.player != null) {
+            if (client.player != null) {
                 ((MinecraftClientAccessor) client).setItemUseCooldown(0);
             }
         });
